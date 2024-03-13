e238270647c30e64d0298334e83001e4a70cfd69
Nick Coyne <nick@codevader.com>
2024-03-14 10:21:31 +1300

    Modal preview

---
+++ previews/pulse/modal_preview.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Pulse
+  class ModalPreview < ViewComponent::Preview
+    # @label Playground
+    # @param title text
+    # @param content text
+    # @param size select {{ Pulse::Modal::SIZE_OPTIONS }}
+    # @param close_button toggle
+    # @param close_on_esc toggle
+    # @param close_on_overlay_click toggle
+    # @param close_on_submit toggle
+    # @param auto_close toggle
+    def playground(
+      title: 'Modal title',
+      content: 'Modal content',
+      footer: 'Footer content',
+      size: Pulse::Modal::DEFAULT_SIZE,
+      close_button: true,
+      close_on_esc: true,
+      close_on_overlay_click: true,
+      close_on_submit: true,
+      auto_close: false
+    )
+      render Pulse::Modal.new(size: size,
+                              close_button: close_button,
+                              close_on_esc: close_on_esc,
+                              close_on_overlay_click: close_on_overlay_click,
+                              close_on_submit: close_on_submit,
+                              auto_close: auto_close) do |component|
+        component.with_title { title }
+        component.with_footer { footer }
+        content
+      end
+    end
+
+    # @label Default options
+    # @param title text
+    # @param content text
+    def default(title: 'Modal title', content: 'Modal content')
+      render Pulse::Modal.new do |component|
+        component.with_title { title }
+        content
+      end
+    end
+  end
+end
